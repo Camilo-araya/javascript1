6c482466c8e5121131c57492f00729e0a35a333e
Camilo Araya Olivera <camilohilda10@gmail.com>
2024-12-22 23:28:02 -0300

    arreglando el archivo

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,24 +1,24 @@
-// Seleccionar elementos del DOM
+
 const botonMas = document.querySelector('.boton-mas');
 const botonMenos = document.querySelector('.boton-menos');
 const campoCantidad = document.querySelector('.cantidad');
 const totalAPagar = document.querySelector('.total');
 
-// Obtener datos iniciales
-let cantidad = parseInt(campoCantidad.textContent) || 0; // Inicializar a 0 si no hay valor
-const precioUnitario = 400000; // Ajusta el precio unitario según tu producto
 
-// Función para actualizar la cantidad y el total
+let cantidad = parseInt(campoCantidad.textContent) || 0; 
+const precioUnitario = 400000; 
+
+
 function actualizarCantidadYTotal() {
-    // Asegurar que la cantidad no sea negativa
+    
     cantidad = Math.max(cantidad, 0);
 
-    // Actualizar el DOM
+    
     campoCantidad.textContent = cantidad;
     totalAPagar.textContent = cantidad * precioUnitario;
 }
 
-// Event listeners para los botones
+
 botonMas.addEventListener('click', () => {
     cantidad++;
     actualizarCantidadYTotal();
@@ -29,5 +29,5 @@ botonMenos.addEventListener('click', () => {
     actualizarCantidadYTotal();
 });
 
-// reinicio del total al cargar la página
+
 actualizarCantidadYTotal();
